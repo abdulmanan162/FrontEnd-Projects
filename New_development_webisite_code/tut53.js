57768303251f2a6d28b27a73aafd2a06d5b7df18
abdulmanan162 <abdulmananishtiaq162@gmail.com>
2021-12-06 18:14:59 +0500

    function in java script

---
+++ New_development_webisite_code/tut53.js
@@ -0,0 +1,30 @@
+console.log ("this tutorial is 53");
+ function greet(name4, greettext="greetext from javascript"){
+     let name1="name1";
+    console.log( name4+   " is a good boy");    
+    console.log(greettext +" "+ name4)
+ }
+function sum(a,b,c){
+    let d=a+b+c;    
+    return d;
+    // the line will execute 
+    // console.log("function is returned");
+}
+
+let name4 ="harry";
+let name1 ="irtash";
+let name2 ="aman";
+let name3="manan";
+let greettext="good morning"
+greet(name1,greettext);
+greet(name2,greettext);
+greet(name3,greettext);
+// let returnval= greet(name4);
+// console.log(returnval)
+
+let returnval= sum(1,2,3);
+console.log(returnval)
+// console.log( name4+   " is a good boy");
+// console.log( name1+   " is a good boy");
+// console.log( name2+   " is a good boy");
+// console.log( name3+   " is a good boy");
