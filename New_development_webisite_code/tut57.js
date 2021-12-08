3c44d13eb4b42c5b8900585459584a6de0487773
abdulmanan162 <abdulmananishtiaq162@gmail.com>
2021-12-08 22:51:08 +0500

    tut57complete

---
+++ New_development_webisite_code/tut57.js
@@ -0,0 +1,27 @@
+
+
+
+    let para =document.getElementById('para');
+
+
+        para.addEventListener("mouseover",function run(){
+    console.log ("mouse inside")
+        });
+
+
+        para.addEventListener("mouseout",function run(){
+     console.log("mouse now went outside")
+        });
+
+
+
+function togglehide(){
+  let btn =document.getElementById('btn');
+  let para =document.getElementById('para');
+  if(para.style.display !="none"){
+  para.style.display=  "none";
+  }
+  else{
+      para.style.display="block";
+  }
+}
